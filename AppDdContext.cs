5db513b15bee3bf0213b6f5a5f295e7fc91877d3
qqniqq <qqniqq@gmail.com>
2025-03-27 08:46:38 +0300

    добавление контекста базы данных в AppDdContext.cs

---
+++ AppDdContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using checkpoint.Models; // Убедитесь, что пространство имен соответствует вашему проекту
+
+namespace checkpoint // Убедитесь, что пространство имен соответствует вашему проекту
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Employer> Employees { get; set; }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
