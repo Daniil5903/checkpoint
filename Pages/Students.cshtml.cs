a7d6cdb466d022e59fc25826eddcc85549f4b0cc
Саид Мужаидов <69said05@gmail.com>
2025-04-18 01:35:01 +0300

    добавил данные на страницу студенты

---
+++ Pages/Students.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using checkpoint.Models;
+using checkpoint.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace checkpoint.Pages
+{
+    public class StudentsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Students { get; set; } = null!;
+
+        public async Task OnGetAsync()
+        {
+            Students = await _context.Students.ToListAsync();
+        }
+
+    }
+}
