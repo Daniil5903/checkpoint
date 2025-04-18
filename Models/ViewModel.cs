27fa01aae7a83f593478126c9f69e8b32c744d63
qqniqq <qqniqq@gmail.com>
2025-04-18 10:48:49 +0300

    создал модель ViewModel для общих полей

---
+++ Models/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿public class PersonEntryViewModel
+{
+    public string PersonType { get; set; } = "Visitor"; // Student / Employee / Visitor
+
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string? Patronymic { get; set; }
+
+    public string? Group { get; set; }         // для студентов
+    public string? Specialty { get; set; }     // для студентов
+    public string? Position { get; set; }      // для работников
+    public string? Purpose { get; set; }       // для посетителей
+}
