46d947996375e0981fd5ec8b0f18094ec16d54d4
qqniqq <qqniqq@gmail.com>
2025-05-06 13:50:21 +0300

    8

--- Models/Visitor.cs
+++ Models/Visitor.cs
@@ -7,7 +7,7 @@ namespace checkpoint.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Имя обязательно для заполнения")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 50 символов")]
-        public string?  Name { get; set; }
+        public string? Name { get; set; }
         [Required(ErrorMessage = "Фамилия обязательна для заполнения")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина фамилии должна быть от 2 до 50 символов")]
         public string? Surname { get; set; }
