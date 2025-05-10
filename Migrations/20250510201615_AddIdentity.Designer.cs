1764fb2db2798be81915408df6830e967c335d6a
Саид Мужаидов <69said05@gmail.com>
2025-05-10 23:24:07 +0300

    добавил новую миграцию для выполнения задачи с авторизацией

--- Migrations/20250510201615_AddIdentity.Designer.cs
+++ Migrations/20250510201615_AddIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using checkpoint.Data;
 
@@ -11,9 +12,11 @@ using checkpoint.Data;
 namespace checkpoint.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250510201615_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
