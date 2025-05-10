04675160003d8a174413dfa3ee7fb63dc5306462
Саид Мужаидов <69said05@gmail.com>
2025-05-10 23:00:03 +0300

    обновил ApplicationDbContext.cs;  csproj; Program.cs для выполнения авторизации

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,23 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using checkpoint.Models;
+﻿using checkpoint.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore; 
+using Microsoft.EntityFrameworkCore;
 
 namespace checkpoint.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AuthUser>
     {
-        //Конструктор, который принимает параметры конфигурации базы данных.
-        // — Передаёт эти параметры в базовый класс DbContext с помощью : base(options).
-        // — Это позволяет настраивать подключение к базе через Dependency Injection.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            //  Удалили Database.Migrate()
         }
-        public DbSet<Employee> Employees { get; set; } = null!;  //таблица Employee, содержащая данные о сотрудниках.
-        public DbSet<Student> Students { get; set; } = null!; //таблица Students, содержащая данные о студентах.
-        public DbSet<Pass> Passes{ get; set; } = null!; //таблица Pass, хранящая информацию о пропусках.
-        public DbSet<CheckpointEmployee> CheckpointEmployees { get; set; } = null!; //таблица CheckpointEmployer, хранящая информацию о сотрудниках КПП.
-        public DbSet<Visitor> Visitors { get; set; } = null!; //таблица Visitor, хранящая информацию о посетителях.
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Student> Students { get; set; } = null!;
+        public DbSet<Pass> Passes { get; set; } = null!;
+        public DbSet<CheckpointEmployee> CheckpointEmployees { get; set; } = null!;
+        public DbSet<Visitor> Visitors { get; set; } = null!;
     }
 }
