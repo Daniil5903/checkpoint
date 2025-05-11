68c4c815092cc75ad815bbf01ff198a5f560140f
Саид Мужаидов <69said05@gmail.com>
2025-05-12 02:28:45 +0300

    конфликт

--- Pages/Account/Logout.cshtml.cs
+++ Pages/Account/Logout.cshtml.cs
@@ -1,23 +1,21 @@
-using checkpoint.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using checkpoint.Models;
 
 namespace checkpoint.Pages.Account
 {
     public class LogoutModel : PageModel
     {
         private readonly SignInManager<AuthUser> _signInManager;
-
         public LogoutModel(SignInManager<AuthUser> signInManager)
         {
             _signInManager = signInManager;
         }
-
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToPage("/Index"); // перенаправление после выхода
+            return RedirectToPage("/Index");
         }
     }
 }
