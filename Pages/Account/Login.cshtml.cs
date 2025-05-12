241f9d57a7d16da3d8762dba6b2b174939c84f50
Саид Мужаидов <69said05@gmail.com>
2025-05-12 03:42:20 +0300

    пробелы

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -13,34 +13,28 @@ namespace checkpoint.Pages.Account
         {
             _signInManager = signInManager;
         }
-
         [BindProperty]
         public InputModel Input { get; set; } = new();
         public string? ErrorMessage { get; set; }
-
         public class InputModel
         {
             [Required]
             [EmailAddress]
             public string Email { get; set; } = string.Empty;
-
             [Required]
             [DataType(DataType.Password)]
             public string Password { get; set; } = string.Empty;
         }
-
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
                 return Page();
 
             var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, false);
-
             if (result.Succeeded)
             {
                 return RedirectToPage("/Index");
             }
-
             ErrorMessage = "Неправильный email или пароль.";
             return Page();
         }
