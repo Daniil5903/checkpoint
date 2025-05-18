02fcc03e76fd139308ed3a2d5b0ffd13d70b4aa9
Саид Мужаидов <69said05@gmail.com>
2025-05-18 17:03:00 +0300

    переделал выход

---
+++ Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using checkpoint.Models;
+
+namespace checkpoint.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<AuthUser> _signInManager;
+        public LogoutModel(SignInManager<AuthUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToPage("/Index");
+        }
+    }
+}
